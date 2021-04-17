f09725b959657139432aad00efd61ca5e7e51594
frankalbenesius <frankalbenesius@gmail.com>
2021-04-17 11:22:12 -0500

    Use production env for production deploy...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 function App() {
   const date = new Date();
   const time = `${date.toTimeString()}`;
-  return <div>this is react {time}!</div>;
+  return (
+    <div>
+      this is react {time}!<p>okokok</p>
+    </div>
+  );
 }
 
 export default App;
